2f8379ff5d4125628b0a8869ac5d99117e43a274
Lunuy <luminuy@gmail.com>
2020-05-05 08:22:22 +0900

    result will be changed when outerChangeable has changed

--- src/Util/fromC.ts
+++ src/Util/fromC.ts
@@ -2,21 +2,23 @@ import Changeable, { O, C, S } from "../Changeable/Changeable";
 import Primitive from "../BuiltIn/Primitive/Primitive";
 
 function fromC<V, T extends Primitive<V>>(changeable : Changeable<T>, primitiveClass? : (new (value : V) => T)) : T {
-    const result = new (primitiveClass ? primitiveClass : (<{new(value : V) : T}>changeable[O].constructor))(changeable[O][O]);
+    const result = new (primitiveClass ? primitiveClass : (<{new(value : V) : T}>changeable[O].constructor))(undefined);
 
     // outer Listener
     const listener = () => {
         lastInner[C].off("set", innerListener);
         lastInner = changeable[O];
+        result.set(lastInner[O]);
         lastInner[C].on("set", innerListener);
     };
     changeable[C].onAny(listener);
 
     // inner Listener
     const innerListener = () => {
-        result.set(changeable[O][O]);
+        result.set(lastInner[O]);
     };
     let lastInner = changeable[O];
+    result.set(lastInner[O]);
     lastInner[C].on("set", innerListener);
 
     result[S] = (() => {
